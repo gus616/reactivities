7de02aa678df830dd89cc80e001a3ccf6e2f21e4
Gustavo Velazquez <gustavo.velazquez.calderon.contractor@bbva.com>
2023-05-17 18:47:19 -0600

    initial config and first endpoints

---
+++ Persistence/Migrations/20230516225148_AddNames.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Value",
+                table: "Activities",
+                newName: "Venue");
+
+            migrationBuilder.RenameColumn(
+                name: "Ttile",
+                table: "Activities",
+                newName: "Title");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Venue",
+                table: "Activities",
+                newName: "Value");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Activities",
+                newName: "Ttile");
+        }
+    }
+}
